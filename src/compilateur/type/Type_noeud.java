9bd4b9be2d1e0822920836d9e2d2c50c17141e7a
TZ-Boomer <74663192+TZ-Boomer@users.noreply.github.com>
2023-09-12 10:08:41 +0200

    Delete src directory

--- src/compilateur/type/Type_noeud.java
+++
@@ -1,9 +0,0 @@
-package type;
-
-public enum Type_noeud {
-	addition, multiplication, division, soustraction,
-	plus_unaire, moins_unaire, constante, not, identificateur, EOF, egal, ou, et, double_egale, modulo,
-	divise, different, inferieur, superieur, pointeur_adresse,
-	vide, bloc, debug, drop, reference, sequence, declaration, block, affectation, condition, loop, BREAK, target, CONTINUE,
-	fonction, appel
-}
