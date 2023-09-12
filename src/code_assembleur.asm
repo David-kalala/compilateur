e484d96031507f9e9de26ed5f9ff181d46c18b82
TZ-Boomer <74663192+TZ-Boomer@users.noreply.github.com>
2023-09-12 10:05:17 +0200

    Delete src directory

--- src/code_assembleur.asm
+++
@@ -1,6 +0,0 @@
-.start 
-.main
-resn 0
-push 0
-ret
-halt 
--- src/compilateur/Type.java
+++
@@ -1,5 +0,0 @@
-package compilateur;
-
-public enum Type {
-	constante, identificateur, plus, moins, exclamation, parenthese_gauche, parenthese_droite;
-}
