6bc5caf7f40242e0fb721fa063e5126cb7637924
David-kalala <81324343+David-kalala@users.noreply.github.com>
2023-10-19 16:43:38 +0200

    Update programmes_tests.c

--- Compilateur_V2/src/programmes_tests.c
+++ Compilateur_V2/src/programmes_tests.c
@@ -83,19 +83,17 @@ int main () {
 }
 
 int main () {
-
-
- 	int t;
+	int t;
  	t = malloc(5); 
-    t[0] = 2;  
-    t[1] = 4;
-    t[2] = 5;
-    t[3] = 6;
-    t[4] = 8;
-   
+	t[0] = 2;
+	t[1] = 4;
+	t[2] = 5;
+	t[3] = 6;
+	t[4] = 8;
    	int b;
-   	
-	for(b = 0; b<5; b = b+1){debug t[b];}
+	for(b = 0; b<5; b = b+1){
+		debug t[b];
+	}
 }
 
 
